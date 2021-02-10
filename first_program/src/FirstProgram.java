a38ee96fa9dbc7f6638fe9d060ccb8b2a6a6f6a7
kaique oliveira santos <kaiquevicio@gmail.com>
2021-02-10 20:24:44 -0300

    Continuação de estudos em java

--- first_program/src/FirstProgram.java
+++ first_program/src/FirstProgram.java
@@ -1,5 +1,14 @@
+import javax.swing.*;
+
 public class FirstProgram {
     public static void main(String[] args) {
+        double baseMenor, baseMaior, h, area;
 
+        baseMenor = 6.0;
+        baseMaior = 8.0;
+        h = 5.0;
+
+        area = (baseMenor + baseMaior) / 2.0 * h;
+        System.out.println(area);
     }
 }
