995cc134831ed6fd272368dbbf6714adbd211199
kaique oliveira santos <kaiquevicio@gmail.com>
2021-02-11 20:03:35 -0300

    Exercícios de fixação

--- first_program/src/Examples.java
+++ first_program/src/Examples.java
@@ -2,25 +2,12 @@ import java.util.Scanner;
 
 public class Examples {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int enteredUser = sc.nextInt();
-
-        String weekDays[] = {"Sunday", "Monday", "Tuesday"};
-
-        //String day;
-        switch (enteredUser) {
-            case 0:
-                //day = "Sunday";
-                System.out.println(weekDays[0]);
-                break;
-            case 1:
-                //day = "Monday";
-                System.out.println(weekDays[1]);
-                break;
-            case 2:
-                //day = "Tuesday";
-                System.out.println(weekDays[2]);
-                break;
+        int x = 0;
+        int y = 5;
+        while(x < 3) {
+            y += 2;
+            x += 1;
         }
+        System.out.println(x + " - " + y);
     }
 }
