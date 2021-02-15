e88ac97a6adec86ec5b5a1f29c1b85d9300058a6
kaique oliveira santos <kaiquevicio@gmail.com>
2021-02-15 15:53:16 -0300

    exemplos de fixação

--- first_program/src/Examples.java
+++ first_program/src/Examples.java
@@ -1,11 +1,34 @@
+import java.util.Scanner;
+
 public class Examples {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char resp;
+        do {
+            System.out.println("Enter with values: ");
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
 
-        int soma = 0;
-        for (int i = 0; i < 5; i++) {
-            soma += i;
-            System.out.println("Valor de i: " + i);
+            int higher = max(a, b, c);
+            showResult(higher);
+            System.out.println("What continues? y/n");
+            resp = sc.next().charAt(0);
+        }while(resp != 'n');
+        sc.close();
+    }
+
+    public static int max(int a, int b, int c) {
+        if(a > b && a > c) {
+            return a;
+        }else if(b > a && b > c) {
+            return b;
+        }else {
+            return c;
         }
-        System.out.println("Soma dos valores: " + soma);
+    }
+
+    public static void showResult(int value) {
+        System.out.println(value);
     }
 }
