e7cecd868b007e6d9c8682c66810852b74f4c65b
kaique oliveira santos <kaiquevicio@gmail.com>
2021-03-15 23:11:06 -0300

    Exercícios de fixação

---
+++ triangle/src/shape/entities/Circle.java
@@ -0,0 +1,27 @@
+package shape.entities;
+
+import shape.enums.Color;
+
+public class Circle extends Shape {
+    private Double radius;
+
+    public Circle(){super();}
+
+    public Circle(Color color, Double radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    public Double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(Double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * Math.pow(this.radius, 2);
+    }
+}
---
+++ triangle/src/shape/enums/Color.java
@@ -0,0 +1,7 @@
+package shape.enums;
+
+public enum Color {
+    BLACK,
+    BLUE,
+    RED
+}
