d5487205d3f50a1c8113863d02a150e81abf1637
kaique oliveira santos <kaiquevicio@gmail.com>
2021-06-10 17:56:25 -0300

    Stack and Unstack

---
+++ data-structure/src/stack/labs/Exers.java
@@ -0,0 +1,44 @@
+package stack.labs;
+
+import java.util.Scanner;
+import stack.Stack;
+
+public class Exers {
+
+    public static void main(String[] args) {
+
+        Stack<Integer> Stack = new Stack<>();
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i=1; i<=10; i++){
+
+            System.out.println("Enter a number:");
+
+            int num = scanner.nextInt();
+
+            if (num % 2 == 0){
+                System.out.println("Staking the number " + num);
+                Stack.push(num);
+            } else {
+
+                Integer unstacked = Stack.pop();
+
+                if (unstacked == null){
+                    System.out.println("Stack is empty");
+                } else {
+                    System.out.println("Odd number, unstacking a Stack element: " + unstacked);
+                }
+            }
+        }
+
+        System.out.println("All numbers have been read, unstacking Stack numbers");
+
+        while (!Stack.isEmpty()){
+            System.out.println("Unstacking a Stack element: " + Stack.pop());
+        }
+
+        System.out.println("All elements were unstacked");
+    }
+
+}
