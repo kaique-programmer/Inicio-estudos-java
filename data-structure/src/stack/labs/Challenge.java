7fc6a64bf9633173b991ee766fa7b02be8adfb1f
kaique oliveira santos <kaiquevicio@gmail.com>
2021-06-10 23:46:17 -0300

    Challenge: Test palindrome

---
+++ data-structure/src/stack/labs/Challenge.java
@@ -0,0 +1,40 @@
+package stack.labs;
+
+import stack.Stack;
+
+public class Challenge {
+    public static void main(String[] args) {
+
+        printResult("ADA");
+
+        printResult("ABCD");
+
+        printResult("ABCCBA");
+
+        printResult("Kaique");
+    }
+
+    public static void printResult(String word){
+        System.out.println(word + " Is palindrome? " + palindromeTest(word));
+    }
+
+    public static boolean palindromeTest(String word){
+
+        Stack<Character> stack = new Stack<Character>();
+
+        for (int i=0; i<word.length(); i++){
+            stack.push(word.charAt(i));
+        }
+
+        String reverseWord = "";
+        while (!stack.isEmpty()){
+            reverseWord += stack.pop();
+        }
+
+        if (reverseWord.equalsIgnoreCase(word)){
+            return true;
+        }
+
+        return false;
+    }
+}
