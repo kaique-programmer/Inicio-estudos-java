6b5f5928b8e166553d273f602fc89347485da6c7
kaique oliveira santos <kaiquevicio@gmail.com>
2021-06-12 21:48:28 -0300

    API: PriorityQueue

--- data-structure/src/queue/test/QueueWithPriorityTest.java
+++ data-structure/src/queue/test/QueueWithPriorityTest.java
@@ -3,10 +3,14 @@ package queue.test;
 import queue.QueueWithPriority;
 import queue.entities.Patient;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class QueueWithPriorityTest {
     public static void main(String[] args) {
 
-        QueueWithPriority<Patient> queue = new QueueWithPriority<>();
+        /*QueueWithPriority<Patient> queue = new QueueWithPriority<>();
 
         queue.push(new Patient("A", 2));
         queue.push(new Patient("C", 1));
@@ -16,6 +20,20 @@ public class QueueWithPriorityTest {
 
         System.out.println(queue.remove());
 
-        System.out.println(queue);
+        System.out.println(queue);*/
+
+        Queue<Patient> queueWithPriority = new PriorityQueue<>(
+                new Comparator<Patient>() {
+                    @Override
+                    public int compare(Patient patient1, Patient patient2) {
+                        return Integer.valueOf(patient1.getPriority()).compareTo(patient2.getPriority());
+                    }
+                }
+        );
+
+        queueWithPriority.add(new Patient("A", 2));
+        queueWithPriority.add(new Patient("B", 1));
+
+        System.out.println(queueWithPriority);
     }
 }
